868307eb61963b90ed587cc12369b0d8835737db
Edwin Hermans <edwin@madtech.cx>
2016-03-29 14:24:21 +0200

    Replace troublesome pgbouncer module with more reliable one

--- manifests/pgbouncer.pp
+++ manifests/pgbouncer.pp
@@ -1,40 +1,26 @@
 # Install and configure PgBouncer
 class role_waarneming::pgbouncer (
+  $waarneming_pw = postgresql_password('waarneming', $::role_waarneming::conf::waarneming_password),
+  $local_be_pw   = postgresql_password('local_be', $::role_waarneming::conf::local_be_password),
+  $local_nl_pw   = postgresql_password('local_nl', $::role_waarneming::conf::local_nl_password),
+  $local_xx_pw   = postgresql_password('local_xx', $::role_waarneming::conf::local_xx_password),
 ) {
   # Install PostgreSQL client software
   class { '::postgresql::client':
     require => Class['postgresql::globals'],
   }->
   class { '::pgbouncer':
-    config_params => {
-      'listen_port' => $::role_waarneming::conf::pgbouncer_port,
-      'admin_users' => 'hugo,hugoadmin,dylan,dylanadmin,hisko,hiskoadmin',
-      'auth_type'   => 'md5',
-    },
-    userlist      => [
-      {
-        'user'     => 'waarneming',
-        'password' => $::role_waarneming::conf::waarneming_password,
-      },
-      {
-        'user'     => 'local_be',
-        'password' => $::role_waarneming::conf::local_be_password,
-      },
-      {
-        'user'     => 'local_nl',
-        'password' => $::role_waarneming::conf::local_nl_password,
-      },
-      {
-        'user'     => 'local_xx',
-        'password' => $::role_waarneming::conf::local_xx_password,
-      },
+    listen_port => $::role_waarneming::conf::pgbouncer_port,
+    databases   => [ "* = host=${$::role_waarneming::conf::db_host}", ],
+    admin_users => 'hugo,hugoadmin,dylan,dylanadmin,hisko,hiskoadmin',
+    auth_type   => 'md5',
+    auth_list   => [
+      "\"waarneming\" \"${waarneming_pw}\"",
+      "\"local_be\" \"${local_be_pw}\"",
+      "\"local_nl\" \"${local_nl_pw}\"",
+      "\"local_xx\" \"${local_xx_pw}\"",
     ],
-    databases     => [
-      {
-        'source_db' => $::role_waarneming::conf::db_name,
-        'host'      => $::role_waarneming::conf::db_host,
-        'dest_db'   => $::role_waarneming::conf::db_name,
-      },
-    ],
+    pool_mode   => 'session',
+    pidfile     => '/var/run/postgresql/pgbouncer.pid',
   }
 }
