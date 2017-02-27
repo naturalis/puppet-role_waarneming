638a4e0bb3847f8fda36c5dc8f6804c761a30374
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2017-02-27 14:20:15 +0100

    Update iostat.sh

--- files/analytics/iostat.sh
+++ files/analytics/iostat.sh
@@ -1,2 +1,5 @@
+# crontab: 
+# */1 * * * * /bin/bash /opt/postgresql/iostat.sh
+
 #!/bin/bash
 iostat -x -m -y 5 12 | awk '/vda/ { print strftime("%Y-%m-%dT%H:%M:%S"), $0; fflush(); }' >> /opt/postgresql/iostat.plain
