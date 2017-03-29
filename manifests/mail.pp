2c06f6a2ef2c026ba1a0387bd715d0af6fc55d38
Edwin Hermans <edwin@madtech.cx>
2017-03-29 11:41:49 +0200

    set correct domain as origin, fix minor typo in metadata
    Signed-off-by: Edwin Hermans <edwin@madtech.cx>

--- manifests/mail.pp
+++ manifests/mail.pp
@@ -2,8 +2,10 @@
 class role_waarneming::mail (
 ) {
   # Install postfix
-  # Configure to listen only on local interfaces
+  # listen only on local interfaces
+  # set domain as origin
   class { '::postfix':
     inet_interfaces => '127.0.0.1, [::1]',
+    myorigin        => 'waarneming.nl',
   }
 }
