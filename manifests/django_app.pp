0123d5294ecaca5a06e12a6e621181e3adb21694
Hugo van Duijn <hugo.vanduijn@naturalis.nl>
2018-07-31 06:53:31 +0000

    updated deployment

--- manifests/django_app.pp
+++ manifests/django_app.pp
@@ -57,6 +57,9 @@ class role_waarneming::django_app (
       "set Cmnd_Alias[alias/name = 'SERVICES']/alias/command[3] '/usr/bin/supervisorctl restart obs'",
       "set Cmnd_Alias[alias/name = 'SERVICES']/alias/command[4] '/usr/bin/puppet agent -t'",
       "set Cmnd_Alias[alias/name = 'SERVICES']/alias/command[5] '/usr/bin/puppet agent -t --debug'",
+      "set Cmnd_Alias[alias/name = ‘SERVICES’]/alias/command[6] ‘/usr/bin/supervisorctl start obs-worker’",
+      "set Cmnd_Alias[alias/name = ‘SERVICES’]/alias/command[7] ‘/usr/bin/supervisorctl stop obs-worker’",
+      "set Cmnd_Alias[alias/name = ‘SERVICES’]/alias/command[8] ‘/usr/bin/supervisorctl restart obs-worker’",
       "set spec[user = 'obs']/user obs",
       "set spec[user = 'obs']/host_group/host ALL",
       "set spec[user = 'obs']/host_group/command SERVICES",
@@ -113,8 +116,8 @@ class role_waarneming::django_app (
   }
 
   # Configure postgres user credentials in app
-  file { '/home/obs/django/app/settings_local.py':
-    content => template('role_waarneming/settings_local.py.erb'),
+  file { '/home/obs/django/.env':
+    content => template('role_waarneming/.env.erb'),
     replace => $::role_waarneming::conf::obs_managesettings,
     require => Vcsrepo['/home/obs/django'],
   }
@@ -129,12 +132,30 @@ class role_waarneming::django_app (
     require => Class['apt::update'],
   }
 
+  ensure_packages(['libjpeg-dev','libpng12-dev','rabbitmq-server'])
+
+  apt::source { 'deadsnakes':
+    location => 'http://ppa.launchpad.net/deadsnakes/ppa/ubuntu/',
+    release  => 'xenial',
+    repos    => 'main',
+    key      => {
+      'id'     => 'F23C5A6CF475977595C89F51BA6932366A755776',
+      'server' => 'keyserver.ubuntu.com',
+    },
+    notify   => Exec['apt_update']
+  }
+  package {['python3.7-minimal', 'python3.7-dev']:
+    require   => Apt::Source['deadsnakes']
+  }
+
   # Install python, python-dev, virtualenv and create the virtualenv
   class { '::python':
     dev        => present,
     virtualenv => present,
   }->
-  ::python::virtualenv { '/home/obs/virtualenv' :
+  python::virtualenv { '/home/obs/virtualenv' :
+    version      => '3.7',
+    distribute   => false,
     ensure       => present,
     requirements => '/home/obs/django/requirements.txt',
     owner        => 'obs',
@@ -142,6 +163,7 @@ class role_waarneming::django_app (
     require      => [
       Vcsrepo['/home/obs/django'],
       Class['postgresql::lib::devel'],
+      Package['libjpeg-dev','libpng12-dev']
     ],
   }
 
@@ -174,6 +196,15 @@ class role_waarneming::django_app (
     notify  => Service['supervisor'],
   }
 
+  file { '/etc/supervisor/conf.d/obs-worker.conf':
+    ensure  => present,
+    owner  => 'root',
+    group  => 'root',
+    source  => 'puppet:///modules/role_waarneming/supervisor_obs-worker.conf',
+    require => Package['supervisor'],
+    notify  => Service['supervisor'],
+  }
+
   # Run migrate to update DB schema
   exec { 'migrate obs':
     command     => 'python manage.py migrate --noinput',
@@ -181,7 +212,7 @@ class role_waarneming::django_app (
     cwd         => '/home/obs/django',
     user        => 'obs',
     require     => [
-      File['/home/obs/django/app/settings_local.py'],
+      File['/home/obs/django/.env'],
       Python::Virtualenv['/home/obs/virtualenv'],
     ],
     before      => Exec['restart obs'],
@@ -196,7 +227,7 @@ class role_waarneming::django_app (
     cwd         => '/home/obs/django',
     user        => 'obs',
     require     => [
-      File['/home/obs/django/app/settings_local.py'],
+      File['/home/obs/django/.env'],
       Python::Virtualenv['/home/obs/virtualenv'],
     ],
     before      => Exec['restart obs'],
