5c32e815b86eb823eb8b37a45736531f27165573
David Heijkamp <david.heijkamp@naturalis.nl>
2020-06-04 17:27:02 +0200

    Set myhostname and fix config typos

--- manifests/mail.pp
+++ manifests/mail.pp
@@ -9,22 +9,27 @@ class role_waarneming::mail (
     myorigin        => 'waarneming.nl',
   }
 
+  postfix::config { 'myhostname':
+    ensure  => present,
+    value   => 'mail.waarneming.nl',
+  }
+
   postfix::config { 'inet_protocols':
     ensure  => present,
     value   => 'ipv4',
   }
 
-  postfix::config { 'smpt_destination_concurrency_limit':
+  postfix::config { 'smtp_destination_concurrency_limit':
     ensure  => present,
     value   => '2',
   }
 
-  postfix::config { 'smpt_destination_rate_delay':
+  postfix::config { 'smtp_destination_rate_delay':
     ensure  => present,
     value   => '1s',
   }
 
-  postfix::config { 'smpt_extra_recipient_limit':
+  postfix::config { 'smtp_extra_recipient_limit':
     ensure  => present,
     value   => '10',
   }
